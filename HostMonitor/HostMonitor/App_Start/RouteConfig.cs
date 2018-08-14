398c96c922e602efe306455268e9bf6e08909a1c
Luis Moraes <moraes.luishenrique17@gmail.com>
2018-08-14 12:52:48 -0300

    Layout

--- HostMonitor/HostMonitor/App_Start/RouteConfig.cs
+++ HostMonitor/HostMonitor/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace HostMonitor
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "hosts", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
