f1e637302eee961d0c8e114a595316a2a792c6b1
Luis Moraes <moraes.luishenrique17@gmail.com>
2018-09-03 16:07:46 -0300

    teste

---
+++ AJ/AJ/Models/Grupo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AJ.Models
+{
+    public class Grupo
+    {
+
+        public int id { get; set; }
+        public string nome { get; set; }
+
+
+
+
+    }
+}
---
+++ AJ/AJ/Models/Subgrupo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AJ.Models
+{
+    public class Subgrupo
+    {
+
+        public int id { get; set; }
+        public int groupid { get; set; }
+        public string name { get; set; }
+
+       
+      
+    }
+}
